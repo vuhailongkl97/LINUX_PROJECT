cf0cd4709974295c8da94f01193f930625ab5a3d
long linux mint <vuhailongkl97@gmail.com>
2019-12-04 20:51:26 +0700

    first stable version communicate via uart

---
+++ DASAN/game_ar/app_code/mouse_movement/MPU6050_DMP6/MPU6050_DMP6.ino
@@ -0,0 +1,400 @@
+  // I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
+// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//      2019-07-08 - Added Auto Calibration and offset generator
+//		   - and altered FIFO retrieval sequence to avoid using blocking code
+//      2016-04-18 - Eliminated a potential infinite loop
+//      2013-05-08 - added seamless Fastwire support
+//                 - added note about gyro calibration
+//      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
+//      2012-06-20 - improved FIFO overflow handling and simplified read process
+//      2012-06-19 - completely rearranged DMP initialization code and simplification
+//      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
+//      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
+//      2012-06-05 - add gravity-compensated initial reference frame acceleration output
+//                 - add 3D math helper file to DMP6 example sketch
+//                 - add Euler output and Yaw/Pitch/Roll output formats
+//      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
+//      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
+//      2012-05-30 - basic DMP initialization working
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050_6Axis_MotionApps_V6_12.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+ * ========================================================================= */
+
+/* =========================================================================
+   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
+   when using Serial.write(buf, len). The Teapot output uses this method.
+   The solution requires a modification to the Arduino USBAPI.h file, which
+   is fortunately simple, but annoying. This will be fixed in the next IDE
+   release. For more info, see these links:
+
+   http://arduino.cc/forum/index.php/topic,109987.0.html
+   http://code.google.com/p/arduino/issues/detail?id=958
+ * ========================================================================= */
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+//#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+enum command {COMMAND_REQUEST_PITCH_YAW = 0};
+enum mouse_ev { mIDLE = 0, LEFT_CLICK  = 1, RIGHT_CLICK = 2 , STOP_MOVE = 4};
+enum orientation_character {oIDLE = 0, LEFT = 1 , GO_AHEAD = 2 , RIGHT = 4, BACK = 8};
+int mouse_click_state =  mIDLE;
+int orientation = oIDLE; 
+
+const int button_sp = 3;
+const int btn_left_mouse = 4;
+//get state value
+unsigned long last_btn_sp = 0; 
+unsigned long last_btn_shot = 0; 
+int OX = A0;
+int OY = A1;
+
+int mouse_get_state()
+{
+	static int m_state = mIDLE;
+	// check button here 
+  if ( millis() - last_btn_sp > 100)
+  {
+    m_state &= ~(1u << 2);
+    m_state |= (!(digitalRead(button_sp)) << 2);
+    
+    last_btn_sp = millis();
+  }
+  if ( millis() - last_btn_shot > 100)
+  {
+    m_state &= ~( 1u << 0);
+    //m_state |= ((!digitalRead(btn_left_mouse)) << 0);
+    last_btn_shot = millis();
+  }
+	return m_state;
+}
+int orientation_get_state()
+{
+	static int o_state = oIDLE;
+  int OX_value , OY_value;
+  
+  OX_value = analogRead(OX);
+  OY_value = analogRead(OY);
+
+  
+	//check joystick here 
+  if ( OX_value > 700) 
+  {
+    o_state |=  RIGHT;
+  }
+  else if( OX_value < 300) 
+  {
+    o_state |= LEFT;
+  }
+  else{
+    o_state &= ~LEFT;
+    o_state &= ~RIGHT;
+  }
+  if ( OY_value > 700) 
+  {
+    o_state |=  GO_AHEAD;
+  }
+  else if( OY_value < 300) 
+  {
+    o_state |= BACK;
+  }
+  else 
+  {
+    o_state &= ~GO_AHEAD;
+    o_state &= ~BACK;
+  }
+	return o_state;
+}
+
+//end get state value
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+char tmp;
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+  /*init button support mouse , button left mouse*/
+    pinMode(button_sp, INPUT);
+    pinMode(btn_left_mouse, INPUT);
+
+    /*end init some button support*/
+    
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+  //  Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    //Serial.println(F("Testing device connections..."));
+    //Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+   // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+   // while (Serial.available() && Serial.read()); // empty buffer
+   // while (!Serial.available());                 // wait for data
+   // while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    //Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+      //  Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        //Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        //Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        //Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        //Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        if (mpuInterrupt && fifoCount < packetSize) {
+          // try to get out of the infinite loop 
+          fifoCount = mpu.getFIFOCount();
+        }  
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+	if(fifoCount < packetSize){
+	        //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+			// This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+	}
+    // check for overflow (this should never happen unless our code is too inefficient)
+    else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+      //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+
+        // read a packet from FIFO
+	while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
+		mpu.getFIFOBytes(fifoBuffer, packetSize);
+		// track FIFO count here in case there is > 1 packet available
+		// (this lets us immediately read more without waiting for an interrupt)
+		fifoCount -= packetSize;
+	}
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+   mouse_click_state = mouse_get_state();
+   orientation       = orientation_get_state();
+
+
+    if (Serial.available() > 0)
+  {
+     tmp = Serial.read()-'0';
+    // str_tmp = Serial.readString();
+      //Serial.println(str_tmp);
+      tmp = COMMAND_REQUEST_PITCH_YAW;
+      switch (tmp) 
+      {
+        case COMMAND_REQUEST_PITCH_YAW:
+          /*
+           * structure data pit  yaw  rolll <buuton> movement
+           * yaw , pit , roll is float value 
+           * <button> is event (mouse click left click )
+           * movement : control character movement left right , go ahead , back 
+          
+          */
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print(" ");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print(" ");
+            Serial.print(ypr[2] * 180/M_PI);
+            Serial.print(" ");
+            Serial.print(mouse_click_state);
+            Serial.print(" ");
+            Serial.println(orientation);
+           
+          //Serial.println("request pitch yaw");
+          break;
+        default:
+          
+          Serial.println("incorrect request command");
+          break;  
+      }
+}
+}
--- DASAN/game_ar/app_code/mouse_movement/test/get_data_test/t_get_data.c
+++ DASAN/game_ar/app_code/mouse_movement/test/get_data_test/t_get_data.c
@@ -40,35 +40,69 @@ void control(FILE *fp, mouse *x,game_obj *go)
 	int opt = 0;
 	static int timeout = 0;
 	static int change_dev = 0 ;
+	/*variable check support mouse button*/
+	static int is_sp_mouse = 0;
+	static int x_old_pos , y_old_pos;
+	static int old_movement_state = 0;
 	pdata p;
 	get_data(fp,&p);
+
+	/*variable switch enable shot*/
 	static int ready_change_dev = 1;
 	//printf("pitch %f yaw %f roll %f\n",p.pitch, p.yaw, p.roll);
 
-	switch (p.mouse_state)
+	if (mIDLE  != p.mouse_state)
 	{
-		case LEFT_CLICK:
+		if (LEFT_CLICK & p.mouse_state != 0)
+		{
 			x->click(x);			
-			break;
-		case RIGHT_CLICK:
-		case STOP_MOVE:
-		default:
-			break;
+		}
+		if (RIGHT_CLICK & p.mouse_state)
+		{
+
+		}
+
+	 	if (STOP_MOVE & p.mouse_state)
+		{
+			// button is pressed	
+			if ( 0 == is_sp_mouse )
+			{
+				is_sp_mouse = 5;
+				x_old_pos = x->x_current;
+				y_old_pos = x->y_current;
+			}
+			if (5 == is_sp_mouse)
+			{
+			     int tmp = RATIO*p.pitch;
+			     int step = abs(tmp -x_old_pos);
+			     if ( tmp > x_old_pos)
+			     	move_mouse_n_pixel(x, M_RIGHT, step);
+			     else if( tmp < x_old_pos)
+			     	move_mouse_n_pixel(x, M_LEFT, step);
+
+			     //printf("chenh lech %d %d\n", x_old_pos ,tmp);
+
+			     //printf("button support is pressed x_old_pos %d x current %f\n", x_old_pos, p.pitch);
+			}
+
+		}
 	}
-	
 	if (p.mouse_state != STOP_MOVE)
 	{
 		int retx, rety;
+
+		is_sp_mouse = 0;
 		caculator_velocity(fp, x);
 		get_target(x->x_current,x->y_current,x->current_vx, x-> current_vy, &retx, &rety);
 		
 	        //printf("xret = %d , yret = %d vx %f vy %f\n", retx, rety,x->current_vx, x->current_vy);
+	        //printf("currentx = %d , currenty = %d \n",x->x_current, x->y_current);
 		//retx = retx > 400 ? 400 : retx;
 		//retx = retx < -400 ? -400 : retx;
 		//rety = rety > 400 ? 400 : rety;
 		//rety = rety < -400 ? -400: rety;
 		
-		printf("p.roll %f\n" , p.roll);
+		//printf("p.roll %f\n" , p.roll);
 		/*check change but*/
 		
 		if ( abs(p.roll) < 3)
@@ -98,31 +132,35 @@ void control(FILE *fp, mouse *x,game_obj *go)
 
 		x->move(x, retx, rety, 1, 1);	
 	        	
-		dont_move:
-		switch (p.movement_state)
-		{
-			case  LEFT :
-				press_key(go, KEY_A,1); 
-				press_key(go, KEY_A,0); 
-				break;
-			case  RIGHT :
-				press_key(go, KEY_D,1); 
-				press_key(go, KEY_D,0); 
-				break;
-			case  GO_AHEAD :
-				press_key(go, KEY_W,1); 
-				press_key(go, KEY_W,0); 
-				break;
-			case  BACK :
-				press_key(go, KEY_S,1); 
-				press_key(go, KEY_S,0); 
-				break;
-			default:
-				//fprintf(stderr, "movement incorrect %s %d ", __FUNCTION__, __LINE__);
-				break;
-		}
 	}
 
+	dont_move:
+	{
+		// should release or  keep state button
+
+		// hien tai co truoc day ko co 
+		if ((LEFT & p.movement_state) &&  !(LEFT & old_movement_state)) 
+			press_key(go, KEY_A,1); 
+		else if (!(LEFT & p.movement_state) && (LEFT & old_movement_state))
+			press_key(go, KEY_A,0); 
+
+		if ( (RIGHT & p.movement_state) &&  !(RIGHT & old_movement_state))
+			press_key(go, KEY_D,1); 
+		else if( !(RIGHT & p.movement_state) &&  (RIGHT & old_movement_state))
+			press_key(go, KEY_D,0); 
+
+		if ((GO_AHEAD & p.movement_state) &&  !(GO_AHEAD & old_movement_state))
+			press_key(go, KEY_W,1); 
+		else if( !(GO_AHEAD & p.movement_state) &&  (GO_AHEAD & old_movement_state))
+			press_key(go, KEY_W,0); 
+
+		if ((BACK & p.movement_state) &&  !(BACK & old_movement_state))
+			press_key(go, KEY_S,1); 
+		else if ( !(BACK & p.movement_state) &&  (BACK & old_movement_state))
+			press_key(go, KEY_S,0); 
+
+		old_movement_state = p.movement_state;
+	}
 }
 int main(int argc, char *argv[])
 {
